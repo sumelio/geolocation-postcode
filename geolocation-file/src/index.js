54f2ce444eaf7b6acaee8bcef3c9ef24350a3672
Freddy Lemus <freddylemus@orioninfiniti.com>
2020-11-28 11:38:03 -0500

    Test share volumne docker

--- geolocation-file/src/index.js
+++ geolocation-file/src/index.js
@@ -1,1 +1,13 @@
-console.log('Geolocation file')
+const fs = require('fs').promises;
+
+const data = "My data from geolocation-file!";
+
+ async function writeTest() {
+    try {
+        await fs.writeFile('/usr/src/app/files/postcode.txt', data);
+      } catch (error) {
+        console.log(error)
+      }
+}
+
+writeTest()
--- geolocation-postcode/src/index.js
+++ geolocation-postcode/src/index.js
@@ -1,1 +1,17 @@
 console.log('Geolocation postcode ....')
+
+const fs = require('fs').promises;
+
+async function read() {
+    await new Promise((resolve, reject) => {
+        setTimeout(() => resolve('done'), 10000)
+      })
+    try {
+      const data = await fs.readFile('/usr/src/app/files/postcode.txt');
+      console.log(' data in postcode: ' + data); 
+    } catch (error) {
+      console.log(error)
+    }
+}
+
+read()
