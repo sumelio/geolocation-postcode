c56138681aecc46081715f4030e8d4f844f764e5
Freddy Lemus <freddylemus@orioninfiniti.com>
2020-11-29 21:14:52 -0500

    Delete dist folder

--- geolocation-postcode/dist/config/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_POSTCODE_OI = exports.PORT = exports.PATH_FILE = void 0;
-exports.PATH_FILE = process.env.FILES_PATH || '/Users/orioninfiniti/Documents/test/geolocation-postcode/geolocation-postcode/tmp/';
-exports.PORT = Number(process.env.PORT || 8200);
-exports.API_POSTCODE_OI = process.env.API_POSTCODE_OI || 'https://postcodes.io/postcodes';
-//# sourceMappingURL=index.js.map
--- geolocation-postcode/dist/services/integrate.service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendFileProcessId = void 0;
-const sendFileProcessId = (fileProcessId) => {
-    console.log('Start to call executePostcode...' + fileProcessId + ' ' + process.env.GEOLOCATION_POSTCODE);
-};
-exports.sendFileProcessId = sendFileProcessId;
-//# sourceMappingURL=integrate.service.js.map
